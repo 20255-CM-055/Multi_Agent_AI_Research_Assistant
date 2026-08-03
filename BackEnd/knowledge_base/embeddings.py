7fa0370972408a06469069c4f5bbabdac69dfede
20255-CM-055 <nandini.satyaa@gmail.com>
2026-08-03 11:05:41 +0530

    Lazy load sentence transformer model

--- BackEnd/knowledge_base/embeddings.py
+++ BackEnd/knowledge_base/embeddings.py
@@ -1,24 +1,47 @@
+
+# originalll
+
 # from sentence_transformers import SentenceTransformer
 
 # _model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
 # def generate_embedding(text: str):
-#     """
-#     Generate an embedding vector for the given text.
-#     """
 #     return _model.encode(text).tolist()
 
+
+# def generate_embeddings(texts: list[str]):
+#     return _model.encode(texts).tolist()
+
+
+
+
+
+
+
+
+
+# ----------------------------------------
 
 
 from sentence_transformers import SentenceTransformer
 
-_model = SentenceTransformer("all-MiniLM-L6-v2")
+_model = None
+
+
+def get_model():
+    global _model
+
+    if _model is None:
+        print("Loading embedding model...")
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    return _model
 
 
 def generate_embedding(text: str):
-    return _model.encode(text).tolist()
+    return get_model().encode(text).tolist()
 
 
 def generate_embeddings(texts: list[str]):
-    return _model.encode(texts).tolist()
+    return get_model().encode(texts).tolist()
